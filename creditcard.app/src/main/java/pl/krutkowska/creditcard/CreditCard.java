88041724c79c7c8793f80894ac69664e62f77ec0
Albert Stapor <askooler1@gmail.com>
2018-10-25 08:16:52 +0000

    Implement blocking scenario

--- creditcard.app/src/main/java/pl/krutkowska/creditcard/CreditCard.java
+++ creditcard.app/src/main/java/pl/krutkowska/creditcard/CreditCard.java
@@ -1,11 +1,23 @@
 package pl.krutkowska.creditcard;
 
 class CreditCard {
+    
+    private boolean blockade = false;
+    
     public void assignLimit(double limit) {
         
     }   
     
     public double getLimit() {
         return 2000;
+    }
+    
+    public void block() {
+        this.blockade = true;
+    }
+    
+    public boolean isBlocked() {
+        
+        return this.blockade;
     }
 }
